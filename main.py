03f03363aec3262d30429ecca1d10e39575f0562
tfabiha <tfabiha@stuy.edu>
2019-04-01 09:58:41 -0400

    just pushing gallery file

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ edges = []
 polygons = []
 transform = new_matrix()
 
-parse_file( 'script', edges, polygons, transform, screen, color )
+parse_file( 'kat', edges, polygons, transform, screen, color )
+
